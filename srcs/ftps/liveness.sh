9a340a72b145e69fdf04564189d5c216904d1b3c
Ghali Boucetta <gboucett@student.42.fr>
2020-07-08 15:50:55 +0200

    ftps added and working

--- srcs/ftps/liveness.sh
+++ srcs/ftps/liveness.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ $(ps -ef | grep "ftps" | wc -l) -ge 2 ]; then
+if [ $(ps -ef | grep "vsftpd" | wc -l) -ge 2 ]; then
 	echo "Everything is fine !"
 else
 	echo "Something is stopped..." && exit 1
