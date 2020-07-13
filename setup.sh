97fcc30b99e40b8742cbf1e5790c7eab427059f0
Ghali Boucetta <gboucett@student.42.fr>
2020-07-14 01:00:14 +0200

    influxdb and grafana working but not linked yet

--- setup.sh
+++ setup.sh
@@ -18,29 +18,33 @@ echo "           / _| |_     ___  ___ _ ____   _(_) ___ ___  ___               "
 echo "          | |_| __|   / __|/ _ \\ '__\\ \\ / / |/ __/ _ \\/ __|          "
 echo "          |  _| |_    \\__ \\  __/ |   \\ V /| | (_|  __/\\__ \\         "
 echo "          |_|  \\__|___|___/\\___|_|    \\_/ |_|\\___\\___||___/         "
-echo "                 |_____|"
+echo "                 |_____|                                                 "
 echo "========================================================================="
 echo "                                                              by gboucett"
 
 #starting minikube and the dashboard
 minikube start --vm-driver=docker --extra-config=apiserver.service-node-port-range=20-32767
 minikube addons enable dashboard
+minikube addons enable metrics-server
 
 eval $(minikube docker-env)
 
 CLUSTER_IP=$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)
-# echo "Cluster IP : ${CLUSTER_IP}"
 
 echo "Building nginx image..."
-docker build -t custom/nginx srcs/nginx --quiet
+docker build -t custom/nginx srcs/nginx --quiet > /dev/null
 echo "Building ftps image..."
-docker build -t custom/ftps srcs/ftps --quiet
+docker build -t custom/ftps srcs/ftps --quiet > /dev/null
 echo "Building mysql image..."
-docker build -t custom/mysql srcs/mysql --quiet
+docker build -t custom/mysql srcs/mysql --quiet > /dev/null
 echo "Building phpmyadmin image..."
-docker build -t custom/phpmyadmin srcs/phpmyadmin --quiet
+docker build -t custom/phpmyadmin srcs/phpmyadmin --quiet > /dev/null
 echo "Building wordpress image..."
-docker build -t custom/wordpress srcs/wordpress
+docker build -t custom/wordpress srcs/wordpress --quiet > /dev/null
+echo "Building influxdb image..."
+docker build -t custom/influxdb srcs/influxdb
+echo "Building grafana image..."
+docker build -t custom/grafana srcs/grafana
 
 echo "Applying yaml files"
 kubectl apply -f srcs/metallb.yaml
@@ -49,11 +53,15 @@ kubectl apply -f srcs/ftps.yaml
 kubectl apply -f srcs/mysql.yaml
 kubectl apply -f srcs/phpmyadmin.yaml
 kubectl apply -f srcs/wordpress.yaml
+kubectl apply -f srcs/influxdb.yaml
+kubectl apply -f srcs/grafana.yaml
 
 end=`date +%s`
 total=$((end - start))
 echo "========================================================================="
 echo "✅ ft_services deployment done in $total seconds"
 
+echo "Cluster IP : ${CLUSTER_IP}"
 echo "SSH (password : gboucett): ssh -p 2222 gboucett@${CLUSTER_IP}"
 echo "FTPS : ip = ${CLUSTER_IP}, username = gboucett, password = gboucett"
+echo "phpmyadmin : link = http://${CLUSTER_IP}:5000, username = gboucett, password = gboucett"
---
+++ srcs/influxdb/start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+openrc default
+rc-update add influxdb default
+rc-service influxdb start
+sleep 5
+influx -execute "CREATE DATABASE telegraf"
+# influx -execute "CREATE USER $USER WITH PASSWORD 'pw'"
+# influx -execute "GRANT ALL ON telegraf TO $USER"
+# influx -execute "CREATE RETENTION POLICY one_week ON telegraf DURATION 168h REPLICATION 1 DEFAULT"
+# influx -execute "SHOW DATABASES"
+rc-service influxdb stop
+
+influxd
